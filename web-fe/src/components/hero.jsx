9bf144af1d05b3ad59bfb6ae412b6a63995db029
Vishal Gautam <ivishalgautamm@gmail.com>
2024-10-30 11:09:15 +0530

    fixed some css in hero section

--- web-fe/src/components/hero.jsx
+++ web-fe/src/components/hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
   return (
     <div className="bg-gradient-to-b from-primary-25 to-primary-50">
       <section className="mx-auto max-w-screen-xl items-center px-4 pb-24 pt-24 md:px-8 lg:flex">
-        <div className="flex-1 space-y-4 sm:text-center lg:text-left">
+        <div className="flex-1 space-y-5 sm:text-center lg:text-left">
           <h1 className="text-4xl font-bold text-gray-800 xl:text-4xl">
             Your Success, Guided by
             <br />
@@ -22,12 +22,14 @@ export default function Hero() {
             building strong fundamentals, enhancing problem-solving skills, and
             boosting confidence for long-term success.
           </p>
-          <p className="py-3 text-gray-800">What you want to learn?</p>
-          <form className="items-center space-y-3 sm:flex sm:justify-center sm:space-x-3 sm:space-y-0 lg:justify-start">
-            <div className="basis-4/5">
-              <SearchModal />
-            </div>
-          </form>
+          <div className="space-y-2">
+            <p className=" text-gray-800">What you want to learn?</p>
+            <form className="items-center sm:flex sm:justify-center sm:space-x-3 sm:space-y-0 lg:justify-start">
+              <div className="basis-4/5">
+                <SearchModal />
+              </div>
+            </form>
+          </div>
         </div>
         <div className="mt-4 flex-1 text-center lg:ml-3 lg:mt-0">
           <Image
